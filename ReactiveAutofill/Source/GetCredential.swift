4ee93c5e27676eee83d3f217868ab06ba7f32129
John Hearn <john@Johns-MacBook-Pro.local>
2017-09-22 06:50:01 -0700

    initial commit

---
+++ ReactiveAutofill/Source/GetCredential.swift
@@ -0,0 +1,15 @@
+//  GetCredential.swift
+//  ReactiveAutofill
+//
+//  Created by John Hearn on 9/21/17.
+//  Copyright © 2017 John Hearn. All rights reserved.
+//
+
+import Foundation
+import Result
+import ReactiveSwift
+import ReactiveCocoa
+
+struct GetCredential {
+    
+}
